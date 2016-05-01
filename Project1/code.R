b1d57e1ade1ee66e211b649d77a7f06a849d6d8c
jimmy5589 <ahhomem@gmail.com>
2016-05-01 23:59:54 +0200

    MBP Sun May  1 23:59:54 CEST 2016

---
+++ Project1/code.R
@@ -0,0 +1,135 @@
+# How to download a file using a URL
+
+library(downloader) 
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleMiceWeights.csv"
+filename <- "femaleMiceWeights.csv" 
+download(url, destfile=filename)
+
+#
+
+femaleMW = read.csv("femaleMiceWeights.csv")
+str(femaleMW)
+femaleMW[12,2]
+femaleMW$Bodyweight[11]
+length(femaleMW$Bodyweight)
+
+mean(femaleMW[femaleMW$Diet=="hf",]$Bodyweight)
+
+set.seed(1)
+sample(13:24, 1)
+femaleMW$Bodyweight[16]
+
+# dplyr
+install.packages("dplyr")
+library(dplyr)
+
+install.packages("downloader")
+library(downloader)
+url="https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/msleep_ggplot2.csv"
+filename <- basename(url)
+download(url,filename)
+
+dat=read.csv("msleep_ggplot2.csv")
+class(dat)
+
+dat = read.csv("femaleMiceWeights.csv")
+str(dat)
+
+# Random VAriables Exercise #1
+dat=read.csv("femaleControlsPopulation.csv")
+
+summary(dat)
+set.seed(1)
+sam=sample(dat$Bodyweight, 5)
+abs(mean(sam)-mean(dat$Bodyweight))
+
+set.seed(5)
+sam=sample(dat$Bodyweight, 5)
+abs(mean(sam)-mean(dat$Bodyweight))
+
+# Conclusion: Abs diff using 2 different samples in a random variable
+
+# Null distributions exeercises
+
+set.seed(1)
+n=10000
+avgs=vector("numeric",n)
+for (i in 1:n){ 
+  avgs[i]=mean(sample(dat$Bodyweight,50))
+}
+
+sum(abs(avgs-mean(dat$Bodyweight))>1)/n
+
+
+#
+
+install.packages("gapminder")
+library(gapminder)
+data("gapminder")
+head(gapminder)
+
+x=gapminder[gapminder$year==1952,]
+head(x)
+summary(x)
+hist(x$lifeExp)
+
+# We can compute F in two ways: the simplest way is to type mean(x <= a). 
+# This calculates the number of values in x which are less than or equal a, 
+# divided by the total number of values in x, in other words the proportion of values 
+# less than or equal to a.
+
+mean(x$lifeExp <= 40)
+
+mean(x$lifeExp <= 60) - mean(x$lifeExp <= 40)
+
+prop = function(q) {
+  mean(x$lifeExp <= q)
+}
+class(prop)
+prop(40)
+
+qs = seq(from=min(x$lifeExp), to=max(x$lifeExp), length=20)
+props=sapply(qs,prop)
+
+plot(qs,props)
+
+# Or
+
+props = sapply(qs, function(q) mean(x$lifeExp <= q))
+
+#Last was an inline function or anonymous
+
+# Compare with the built in function
+plot(ecdf(x$lifeExp))
+
+#
+dat=read.csv("femaleControlsPopulation.csv")
+summary(dat)
+
+set.seed(1)
+
+avgs5=vector("numeric", 1000)
+n=1000
+for (i in 1:n){
+  avgs5[i]=mean(sample(dat$Bodyweight,5))
+}
+avgs50=vector("numeric",1000)
+for (i in 1:n){
+  avgs50[i]=mean(sample(dat$Bodyweight,50))
+}
+
+hist(avgs5)
+hist(avgs50)
+
+mean(avgs50<=25)-mean(avgs50<=23)
+
+z23=(23-23.9)/0.43
+z25=(25-23.9)/0.43
+
+z25-z23
+
+pnorm(25,23.9,0.43)-pnorm(23,23.9,0.43)
+# same as above:
+pnorm(z25)-pnorm(z23)
+# basically the Z is normalized to Normal(0,1)
+
